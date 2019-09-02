7a941e17073e25cfe706c168801fd73297bb5415
tojatos <tojatos@gmail.com>
2019-09-02 18:53:30 +0200

    CRWI script updated

--- CRWI.sh
+++ CRWI.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-SPOJ_PATH="/home/tojatos/Scripts/SPOJ/"
+if [[ -z $1 ]]; then
+  echo "Missing argument."
+  echo "Usage: $(basename "$0") FILENAME"
+  exit 1
+fi
+SPOJ_PATH=$(dirname "$0")"/"
 SAVE_PATH="/tmp/SPOJ/"
-g++ $SPOJ_PATH$1 -o $SAVE_PATH$1
-$SAVE_PATH$1 < "$SPOJ_PATH"input.txt 
+INPUT_FILE="${SPOJ_PATH}"input.txt
+TARGET_FILE=$(basename "${1}")
+mkdir -p ${SAVE_PATH}
+[[ -f "${INPUT_FILE}" ]] || touch "${INPUT_FILE}"
+g++ "${SPOJ_PATH}${TARGET_FILE}" -o "${SAVE_PATH}${TARGET_FILE}"
+time "${SAVE_PATH}${TARGET_FILE}" < "${INPUT_FILE}"
